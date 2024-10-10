5e6ba5fb74c85360d1dcda976c9a6ee6accc171c
skitomie <skitomie@gmail.com>
2024-10-10 09:50:16 +0100

    blogging_api

--- blogging_api/blogging_api/settings.py
+++ blogging_api/blogging_api/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
-    #S'rest_framework_simplejwt',
+    #'rest_framework_simplejwt',
     'blog',
 ]
 
